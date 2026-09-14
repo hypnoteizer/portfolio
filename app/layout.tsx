c623da15061812b68035fba6d57238d41c24b030
hypnoteizer <orkanselcuk@gmail.com>
2026-09-14 05:10:44 +0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +31,9 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        {children}</body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
